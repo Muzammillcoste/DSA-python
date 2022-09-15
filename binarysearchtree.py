1b3a2cbeae83d4cb532ace7db59528c756d7b8f7
muzammillcoste <muzammilrizvi977@gmail.com>
2022-09-15 23:39:41 +0500

    add buildtree func

---
+++ binarysearchtree.py
@@ -0,0 +1,35 @@
+class BinarySearchTree:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+        
+    def add_child(self,child):
+        if child==self.data:
+            return
+        
+        elif child<self.data:
+            if self.left:
+                self.left.add_child(child)
+            else:
+                self.left=BinarySearchTree(child)
+        else:
+            if self.right:
+                self.right.add_child(child)
+            else:
+                self.right=BinarySearchTree(child)
+                
+    
+                
+def buildtree(elements):
+    if len(elements)==0:
+        raise Exception('list is empty')
+    
+    root=BinarySearchTree(elements[0])
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+        
+    return root
+
+tree_list=[10,4,6,7,9,17,18,0,3,-1,3]
+tree=buildtree(tree_list)
