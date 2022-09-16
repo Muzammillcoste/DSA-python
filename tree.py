a779f8a275b3b77a1ef02c64bcfb40eabd54da7a
muzammillcoste <muzammilrizvi977@gmail.com>
2022-09-16 21:29:29 +0500

    add height function

--- tree.py
+++ tree.py
@@ -31,9 +31,14 @@ class Tree:
             return []
         return Tree.Postorder_traversal(self.left)+Tree.Postorder_traversal(self.right)+[self.data]
     
+    def Height(self):
+        if self is None:
+            return 0
+        return 1+max(Tree.Height(self.left), Tree.Height(self.right))
     
 tree_tuple=((1,3,None), 2, ((None, 3, 4), 5, (6, 7, 8)))
 tree = Tree.BuildTree(tree_tuple)
 print(tree.Inorder_traversal())
 print(tree.Preorder_traversal())
 print(tree.Postorder_traversal())
+print(tree.Height())
