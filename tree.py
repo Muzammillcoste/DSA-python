41e74cffdfe652ef6cf164ff85447c5ee7b02356
muzammillcoste <muzammilrizvi977@gmail.com>
2022-09-16 21:25:05 +0500

    add traversl function

--- tree.py
+++ tree.py
@@ -16,9 +16,24 @@ class Tree:
             node=Tree(data)
         return node
     
+    def Inorder_traversal(self):
+        if self is None:
+            return []
+        return Tree.Inorder_traversal(self.left)+[self.data]+Tree.Inorder_traversal(self.right)
     
+    def Preorder_traversal(self):
+        if self is None:
+            return []
+        return [self.data]+Tree.Preorder_traversal(self.left)+Tree.Preorder_traversal(self.right)
     
+    def Postorder_traversal(self):
+        if self is None:
+            return []
+        return Tree.Postorder_traversal(self.left)+Tree.Postorder_traversal(self.right)+[self.data]
     
     
 tree_tuple=((1,3,None), 2, ((None, 3, 4), 5, (6, 7, 8)))
 tree = Tree.BuildTree(tree_tuple)
+print(tree.Inorder_traversal())
+print(tree.Preorder_traversal())
+print(tree.Postorder_traversal())
